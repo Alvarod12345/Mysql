faf494db49842fe1162b8d8bc2fa69311c4c4ccb
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-06-20 20:27:24 -0500

    create table

---
+++ sentencias.sql
@@ -0,0 +1,11 @@
+-- tabla autores
+
+create table autores(
+    autor_id int,
+    nombre varchar(25),
+    apellido varchar(25),
+    genero char(1),
+    fecha_nacimiento date,
+    pais_origen varchar(40)
+
+);
