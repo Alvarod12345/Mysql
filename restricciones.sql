3cd69aea72af210f06fbcdcda9f07fe138b2e1ac
Alvarod12345 <alvarosantisteban56@gmail.com>
2020-06-21 22:50:18 -0500

    db-table-sec4-cf

---
+++ restricciones.sql
@@ -0,0 +1,46 @@
+
+-- no permitiran nulos en las columnas de la tabla
+
+--valor positivo(unsigned) no nulo
+
+--nos da la full fecha al registrarse (current_timestamp)
+
+        DROP DATABASE IF EXISTS libreria;
+        CREATE DATABASE libreria;
+        USE libreria;
+
+        CREATE TABLE autores(
+            autor_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
+            nombre varchar(25) NOT NULL,
+            apellido varchar(25) NOT NULL,
+            seudonimo varchar(50) UNIQUE,
+            genero ENUM('M','F'), -- m o f
+            fecha_nacimiento date NOT NULL,
+            pais_origen varchar(40) NOT NULL,
+            fecha_creacion datetime DEFAULT CURRENT_TIMESTAMP
+
+        );
+
+        CREATE TABLE libros(
+            libro_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            autor_id INT UNSIGNED NOT NULL,
+            titulo VARCHAR(50) NOT NULL,
+            descripcion VARCHAR(250),
+            paginar INTEGER UNSIGNED,
+            fecha_publicacion DATE NOT NULL,
+            fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (autor_id) REFERENCES autores(autor_id)
+            
+        );
+
+        insert into autores (nombre,apellido,seudonimo,genero,fecha_nacimiento,pais_origen) 
+        values  ('lola','lila','loli','f','1998-01-25','Peru'),
+                ('lola','lila','lola','f','1998-01-25','Peru');
+        
+        insert into libros (autor_id,titulo,fecha_publicacion)
+        values  (1, 'educacion','2018-04-24'),
+                (1, 'm learning','2017-06-12'),
+                (2, 'Deep learning','2016-12-17');
+
+        SELECT * FROM autores;
+        SELECT * FROM libros;
